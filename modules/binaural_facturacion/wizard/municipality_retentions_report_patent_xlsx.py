e764c5b82fe3560a7bd481364ca03a6c66a7820e
Luis <luis@binauraldev.com>
2022-03-31 15:39:05 -0400

    issue: Formato Informe de Patente Municipal en Excel (Bs)

--- modules/binaural_facturacion/wizard/municipality_retentions_report_patent_xlsx.py
+++ modules/binaural_facturacion/wizard/municipality_retentions_report_patent_xlsx.py
@@ -56,6 +56,35 @@ class WizardMaxcamComision(models.TransientModel):
         # control_format = workbook.add_format({'align': 'center'})
         porcent_format = workbook.add_format({'num_format': '0.0 %'})
 
+        columns2[3].update({'format': money_format})
+        columns2[4].update({'format': money_format})
+        columns2[5].update({'format': money_format})
+        columns2[6].update({'format': money_format})
+        columns2[7].update({'format': money_format})
+        columns2[8].update({'format': money_format})
+        columns2[9].update({'format': money_format})
+        columns2[11].update({'format': money_format})
+        columns2[13].update({'format': money_format})
+        columns2[14].update({'format': money_format})
+        columns2[15].update({'format': money_format})
+
+        invoice_lines = self.env['account.move.line'].search(
+            [('move_id.invoice_date', '>=', self.date_start), ('move_id.invoice_date', '<=', self.date_end), ('move_id.move_type', 'in', ["out_invoice", 'out_refund']), ('move_id.financial_document', '=', True)])
+
+        invoice_lines = invoice_lines.filtered(
+            lambda l: l.price_unit > 0)
+
+        nc_financial = 0
+        nd_financial = 0
+
+        for line in invoice_lines:
+            _logger.warning(line.price_unit)
+            _logger.warning(line.move_id.financial_document)
+            if line.move_id.move_type == 'out_refund':
+                nc_financial += line.price_unit
+            if line.move_id.move_type == 'out_invoice':
+                nd_financial += line.price_unit
+
         data = datos.values.tolist()
         col3 = len(columns2)-1
         col2 = len(data)+1
@@ -64,26 +93,92 @@ class WizardMaxcamComision(models.TransientModel):
         worksheet2.hide_gridlines(2)
         worksheet2.add_table(
             cells, {'data': data, 'total_row': True, 'columns': columns2, 'autofilter': False})
+
+        worksheet2.write('F'+str(col2+1), nc_financial,money_format)
+        worksheet2.write('G'+str(col2+1), nd_financial,money_format)
+
+        worksheet2.write_array_formula(
+            'D'+str(col2+1), f'=SUM(D2:D{col2})', money_format)
+        worksheet2.write_array_formula(
+            'E'+str(col2+1), f'=SUM(E2:E{col2})', money_format)
+        worksheet2.write_array_formula(
+            'H'+str(col2+1), f'=SUM(H2:H{col2})', money_format)
+        worksheet2.write_array_formula(
+            'I'+str(col2+1), f'=SUM(I2:I{col2})', money_format)
+        worksheet2.write_array_formula(
+            'J'+str(col2+1), f'=SUM(J2:J{col2})', money_format)
+        worksheet2.write_array_formula(
+            'L'+str(col2+1), f'=SUM(L2:L{col2})', money_format)
+        worksheet2.write_array_formula(
+            'N'+str(col2+1), f'=SUM(N2:N{col2})', money_format)
+        worksheet2.write_array_formula(
+            'O'+str(col2+1), f'=SUM(O2:O{col2})', money_format)
+        worksheet2.write_array_formula(
+            'P'+str(col2+1), f'=SUM(P2:P{col2})', money_format)
+
+        for line in range(2, col2+1):
+            worksheet2.write_array_formula(
+                f'C{line}', f"=D{line}/D{str(col2+1)}", money_format)
+            worksheet2.write_array_formula(
+                f'F{line}', f"=C{line}*F{str(col2+1)}", money_format)
+            worksheet2.write_array_formula(
+                f'H{line}', f"=D{line}-E{line}-F{line}+G{line}", money_format)
+            worksheet2.write_array_formula(
+                f'G{line}', f"=G{str(col2+1)}*C{line}", money_format)
+            worksheet2.write_array_formula(
+                f'I{line}', f"=H{line}*0.9", money_format)
+            worksheet2.write_array_formula(
+                f'J{line}', f"=H{line}-I{line}", money_format)
+            worksheet2.write_array_formula(
+                f'L{line}', f"=I{line}*K{line}/1000", money_format)
+            worksheet2.write_array_formula(
+                f'N{line}', f"=IF(L{line}>M{line};L{line};M{line})", money_format)
+            worksheet2.write_array_formula(
+                f'O{line}', f"=J{line}*K{line}/1000", money_format)
+            worksheet2.write_formula(
+                f'P{line}', f"=N{line}", money_format)
+
         workbook.close()
         data2 = data2.getvalue()
         return data2
 
     def _get_excel_municipality_retention_report(self):
 
-        retentions = self.env['account.municipality.retentions'].search(
-            [('date_accounting', '>=', self.date_start), ('date_accounting', '<=', self.date_end), ('state', '=', 'emitted'), ('type', '=', "out_invoice")], order='date_accounting asc')
+        invoice_lines = self.env['account.move.line'].search(
+            [('move_id.invoice_date', '>=', self.date_start), ('move_id.invoice_date', '<=', self.date_end), ('move_id.move_type', 'in', ["out_invoice", 'out_refund']), ('move_id.financial_document', '=', False)])
+
+        invoice_lines = invoice_lines.filtered(
+            lambda l: any(l.product_id.categ_id.ciu))
+
+        groups = {}
+
+        for line in invoice_lines:
+            if not line.product_id.categ_id.name in groups.keys():
+                groups[line.product_id.categ_id.name] = {
+                    "CIU": line.product_id.categ_id.ciu.name,
+                    "sales_total": line.price_unit if line.move_id.move_type == 'out_invoice' else 0,
+                    "refund_total": line.price_unit if line.move_id.move_type == 'out_refund' else 0,
+                    "aliquot": line.product_id.categ_id.ciu.aliquot,
+                    "minimum_monthly": line.product_id.categ_id.ciu.minimum_monthly,
+                }
+                continue
+
+            groups[line.product_id.categ_id.name]["sales_total"] += line.price_unit if line.move_id.move_type == 'out_invoice' else 0
+            groups[line.product_id.categ_id.name]["refund_total"] += line.price_unit if line.move_id.move_type == 'out_refund' else 0
+
+        _logger.warning(groups)
 
         lista = []
         cols = OrderedDict([
-            ('RUBROS', '12312321'),
+            ('RUBROS', ''),
             ('CIU', ''),
-            ('PRORRATA DEDUCCIONES', ''),
-            ('VENTAS BRUTAS (Factura + ND)', ''),
-            ('DEVOLUC. VENTAS (NC)', ''),
-            ('DSTOS. VENTAS (NC Financiera)', ''),
-            ('NOTAS DE DEBITO (ND financiera)', ''),
-            ('INGRESOS 100%', ''),
-            ('INGRESOS 90%', ''),
+            ('PRORRATA DEDUCCIONES', 0.0),
+            ('VENTAS BRUTAS (Factura + ND)', 0.0),
+            ('DEVOLUC. VENTAS (NC)', 0.0),
+            ('DSTOS. VENTAS (NC Financiera)', 0.0),
+            ('NOTAS DE DEBITO (ND financiera)', 0.0),
+            ('INGRESOS 100%', 0.0),
+            ('INGRESOS 90%', 0.0),
             ('INGRESOS 10%', 0.00),
             ('Alic %', ''),
             ('IMPUESTO', 0.00),
@@ -92,14 +187,20 @@ class WizardMaxcamComision(models.TransientModel):
             ('IMPUESTO RESTANTE 10%', 0.00),
             ('ANTICIPO 90%', 0.00),
         ])
-        currency = self.env.company.currency_id.name
+
         numero = 1
-        for retention in retentions:
-            for retention_line in retention.retention_line_ids:
-                rows = OrderedDict()
-                rows.update(cols)
-                lista.append(rows)
-                numero += 1
+        for line in groups.keys():
+            rows = OrderedDict()
+            rows.update(cols)
+            rows['RUBROS'] = line
+            rows['CIU'] = groups[line]['CIU']
+            rows['VENTAS BRUTAS (Factura + ND)'] = groups[line]['sales_total']
+            rows['DEVOLUC. VENTAS (NC)'] = groups[line]['refund_total']
+            rows['Alic %'] = groups[line]['aliquot']
+            rows['MINIMO TRIBUTABLE'] = groups[line]['minimum_monthly']
+
+            lista.append(rows)
+            numero += 1
         tabla = pandas.DataFrame(lista)
         return tabla.fillna(0)
 
@@ -128,12 +229,3 @@ class ControllerMunicipalityRetentionXlsx(http.Controller):
         return request.make_response(filecontent,
                                      [('Content-Type', 'application/pdf'), ('Content-Length', len(filecontent)),
                                       ('Content-Disposition', content_disposition(name_document+'.xlsx'))])
-
-
-def current_date_format(date):
-    months = ("Enero", "Febrero", "Marzo", "Abri", "Mayo", "Junio", "Julio",
-              "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
-    month = months[date.month - 1]
-    year = date.year
-    message = "{} {}".format(month, year)
-    return message
